66a346ae4fc833eb8ee5599a5185ba108b6ddbbf
Justin Young <justin_dean_young@yahoo.com>
2023-09-16 10:54:16 -0400

    init

---
+++ index.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import chalk from "chalk";
+import express from "express";
+import * as http from "http";
+import { Server } from "socket.io";
+import * as fs from "fs";
+
+let port = 9000;
+let filename = "teldata";
+const app = express();
+const server = http.createServer(app);
+let outputfilename;
+let stream: any;
+
+const program = new Command();
+
+const io = new Server(server, {
+  allowEIO3: true,
+  cors: { credentials: true, origin: "http://localhost" },
+});
+
+program
+  .version("1.0.0")
+  .description("Local Telemetry Server")
+  .option("-p, --port <port>", "accessible port to run server on")
+  .option("-f, --filename <filename>", "filename to write telemetry data to")
+  .action(cmd => {
+    console.log(cmd);
+
+    if (cmd.port) port = cmd.port;
+    if (cmd.filename) filename = cmd.filename;
+
+    outputfilename = `${performance.now()}_${filename}.bin`;
+    stream = fs.createWriteStream(outputfilename, { flags: "w" });
+
+    io.on("connection", socket => {
+      console.log(chalk.blue(`Client connected`));
+      // Send an acknowledgment message to the client
+      socket.emit("acknowledgment", "Server is running and you are connected.");
+
+      socket.on("verification", (msg: string) => {});
+
+      socket.on("datasend", (msg: string) => {
+        stream.write(Buffer.from(msg));
+      });
+
+      socket.on("disconnect", () => {
+        console.log(chalk.blue(`Client disconnected`));
+        stream.end();
+      });
+    });
+
+    server.listen(port, () => {
+      console.log(chalk.blueBright(`Starting Server, port ${port}, and writing to base filename: ${filename}`));
+    });
+  });
+
+program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# README telServer
+
+A local CLI that runs a data collection server locally for the telClient library to connect to
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+
+`npx telServer -p <port number> -f <filename>`
+
+port is 9000 if excluded and filename is teldata if excluded
+
+## Usage
+
+run the CLI command above to start the telserver PRIOR to running your client app...
+
+If the client app cannot find the server, it will not run the telClient telemetry library
+
+## Support
+
+Please [open an issue](https://github.com/fraction/readme-boilerplate/issues/new) for support.
